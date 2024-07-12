ebaaad4c8ba35a99cea04e6b8c9a6d65ccb4d661
LeonKote <articdata@bot.io>
2024-07-12 16:10:39 +0000

    Update from https://github.com/LeonKote/ArticDataGenerator/commit/766a7a496d6749058681f46e429735cae0fb2e24

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 tasks {
     register<Jar>("packageVersion") {
         archiveBaseName.set("articdata")
-        archiveVersion.set("1.17-d411aaed")
+        archiveVersion.set("1.17-766a7a49")
 
         destinationDirectory.set(layout.buildDirectory.dir("dist"))
         from(project.rootProject.rootDir)
@@ -20,7 +20,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "de.articdive"
             artifactId = "articdata"
-            version = "1.17-d411aaed"
+            version = "1.17-766a7a49"
 
             pom {
                 name.set("ArticData")
